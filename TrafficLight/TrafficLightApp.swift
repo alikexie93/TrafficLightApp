a965894805dc2522c9efa4edafa51b8e06bd47f8
aleksei <alikexie93@yandex.ru>
2023-06-10 18:12:17 +0600

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Алексей Филиппов on 10.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
